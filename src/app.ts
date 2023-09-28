402ef0db797a9d80451597eefdc670abbdbb44f0
emachado9202 <emachado9202@gmail.com>
2023-09-28 18:43:07 -0400

    add body parser library

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { router } from './routes';
 import { env } from 'process';
-
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+  
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
 
